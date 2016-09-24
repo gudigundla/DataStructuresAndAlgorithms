104affa749c6eb53f16134d84ccfce3406c4380f
hari.gudigundla <hari@gudigundla.com>
2016-09-24 17:20:41 -0400

    Implemented deleteWord() and findAllWordsWithPrefix() functions of the MyTrie class

--- src/ds/learning/trie/MyTrie.java
+++ src/ds/learning/trie/MyTrie.java
@@ -1,17 +1,17 @@
 package ds.learning.trie;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Created by hari.gudigundla on 16-09-23.
  */
 public class MyTrie {
-
+    char letter;
     Map<Character,MyTrie> children;
     boolean endOfWord;
 
-    public MyTrie(){
+    public MyTrie(char letter){
+        this.letter=letter;
         children=new TreeMap<Character, MyTrie>();
         endOfWord=false;
     }
@@ -24,7 +24,7 @@ public class MyTrie {
                 currentNode=currentNode.children.get(word.charAt(i));
             }
             else{
-                MyTrie temp=new MyTrie();
+                MyTrie temp=new MyTrie(word.charAt(i));
                 currentNode.children.put(word.charAt(i),temp);
                 currentNode=temp;
             }
@@ -36,7 +36,7 @@ public class MyTrie {
     }
 
 
-    //search method
+    //search MyTrie Node method
     public MyTrie searcMyTrieNode(String word){
         MyTrie currentNode=this;
         for(int i=0; i<word.length(); i++){
@@ -49,6 +49,7 @@ public class MyTrie {
        return currentNode;
     }
 
+    //search word
     public boolean searchWord(String word){
         MyTrie lastNode=searcMyTrieNode(word);
         if(lastNode!=null && lastNode.endOfWord){
@@ -57,11 +58,64 @@ public class MyTrie {
 
     }
 
+    //search prefix
     public boolean searchPrefix(String prefix){
         MyTrie lastNode=searcMyTrieNode(prefix);
         if(lastNode!=null)
             return true;
         else return false;
+    }
+
+    public void deleteWord(String word){
+
+        MyTrie currentNode=this;
+        //stack to keep track of all nodes visited
+        Stack<MyTrie> visitedNodes=new Stack<MyTrie>();
+        visitedNodes.push(this);
+        for(int i=0; i<word.length(); i++){
+            if(currentNode.children.get(word.charAt(i)) != null){
+                currentNode=currentNode.children.get(word.charAt(i));
+                visitedNodes.push(currentNode);
+            }
+            //else word doesn't exist
+        }
+
+        if(!currentNode.children.isEmpty()){
+            currentNode.endOfWord=false;
+        }
+        else {
+            while (!visitedNodes.isEmpty()) {
+                MyTrie child = visitedNodes.pop();
+                if (child.letter != '\0') {
+                    MyTrie parent = visitedNodes.peek();
+                    if (child.children.isEmpty())
+                        parent.children.remove(child.letter);
+                }
+            }
+        }
+
+    }
+
+    private void wordsWithCurrentNode(MyTrie node, String prefix,List<String> list){
+
+        for (Map.Entry<Character,MyTrie>  letter: node.children.entrySet()){
+            String tempPrefix= prefix+letter.getKey();
+            if(letter.getValue().endOfWord){
+                list.add(tempPrefix);
+            }
+            wordsWithCurrentNode(letter.getValue(),tempPrefix,list);
+        }
+    }
+
+    public List<String> findAllWordsWithPrefix(String prefix){
+        MyTrie lastNode=searcMyTrieNode(prefix);
+        List<String> wordsWithPrefix = new ArrayList<String>();
+        if(lastNode!=null){
+            if(lastNode.endOfWord)
+                wordsWithPrefix.add(prefix);
+            wordsWithCurrentNode(lastNode, prefix, wordsWithPrefix);
+        }
+        return wordsWithPrefix;
     }
 
 
--- src/ds/learning/trie/TrieTester.java
+++ src/ds/learning/trie/TrieTester.java
@@ -1,5 +1,7 @@
 package ds.learning.trie;
 
+import java.util.List;
+
 /**
  * Created by hari.gudigundla on 16-09-23.
  */
@@ -7,16 +9,34 @@ public class TrieTester {
 
     public static void main(String[] args){
 
-        MyTrie root= new MyTrie();
-        root.insertWord("apple");
+        MyTrie root= new MyTrie('\0');
+        //root.insertWord("apple");
         root.insertWord("ball");
         root.insertWord("cat");
         root.insertWord("catch");
+        root.insertWord("cab");
+        root.insertWord("camera");
+        root.insertWord("cameron");
+        root.insertWord("cameo");
+        root.insertWord("car");
+        root.insertWord("catepillar");
+        root.insertWord("ca");
+        root.insertWord("Apple");
 
         System.out.println(root.searchWord("apple"));
-        System.out.println(root.searchPrefix("app"));
+        System.out.println(root.searchPrefix("App"));
         System.out.println(root.searchWord("cat"));
-        System.out.println(root.searchPrefix("ca"));
+        System.out.println(root.searchPrefix("catch"));
+        System.out.println(root.searchPrefix(" "));
+
+
+        System.out.println(root.searchPrefix("catch"));
+        root.deleteWord("cat");
+        root.deleteWord("cameo");
+        System.out.println(root.searchPrefix("catch"));
+
+        List<String> words=root.findAllWordsWithPrefix("ca");
+        System.out.println(words);
 
     }
 }
