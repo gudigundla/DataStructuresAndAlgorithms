ffc114d0fec2610972a0f8e9ded26159a7f10669
hari.gudigundla <hari@gudigundla.com>
2016-10-08 13:42:02 -0400

    Implemented BST-insert, delete

---
+++ src/ds/learning/tree/BalancedBinaryTree.java
@@ -0,0 +1,9 @@
+package ds.learning.tree;
+
+/**
+ * Created by hari.gudigundla on 16-10-08.
+ */
+public class BalancedBinaryTree {
+
+
+}
--- src/ds/learning/tree/BinaryTree.java
+++ src/ds/learning/tree/BinaryTree.java
@@ -84,6 +84,63 @@ public class BinaryTree {
         }
     }
 
+    public static BinaryTree createBinaryTree() {
+        BinaryTree root=new BinaryTree(0);
+        BinaryTree node1=new BinaryTree(1);
+        BinaryTree node2=new BinaryTree(2);
+        BinaryTree node3=new BinaryTree(3);
+        BinaryTree node4=new BinaryTree(4);
+        BinaryTree node5=new BinaryTree(5);
+        BinaryTree node6=new BinaryTree(6);
+        BinaryTree node7=new BinaryTree(7);
+        BinaryTree node8=new BinaryTree(8);
+
+        root.left=node1;
+        root.right=node2;
+        node1.left=node3;
+        node1.right=node4;
+        node2.left=node5;
+        node2.right=node6;
+        node5.left=node7;
+        //node7.left=node8;
+
+        return root;
+    }
+
+    public static boolean isBalanced(BinaryTree node){
+        //find 2 heights of a node and if they differ by 0/1 return maxHeight, else return -1
+        if(balancedHeight(node)>-1)
+            return true;
+        else return false;
+    }
+
+    private static int balancedHeight(BinaryTree node){
+        if(node==null) return 0;
+        else{
+            int lHeight=balancedHeight(node.left);
+            int rHeight=balancedHeight(node.right);
+
+            if(lHeight>-1 && rHeight>-1) {
+                if (Math.abs(lHeight-rHeight)>1)
+                    return 1 + Math.max(lHeight, rHeight);
+                else return -1;
+            }
+            else return -1;
+
+        }
+    }
+
+    //post order traversal
+    //gives height of tree if root is passed
+    public static int heightOfNode(BinaryTree node){
+        if(node==null) return 0;
+        else{
+            int lHeight= heightOfNode(node.left);
+            int rHeight= heightOfNode(node.right);
+            return 1+ Math.max(lHeight,rHeight);
+        }
+    }
+
     public void addNode(BinaryTree node){
 
     }
